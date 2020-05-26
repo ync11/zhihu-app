5aeea17d1b4566218a339b183b050747eefea93c
ync <735624429@qq.com>
2020-05-26 15:42:24 +0800

    话题数据填充

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,12 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+
+$factory->define(App\Topic::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'bio' => $faker->paragraph,
+        'questions_count' => 1
+    ];
+});
